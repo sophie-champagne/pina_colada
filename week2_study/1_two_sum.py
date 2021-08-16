399d52fb3c122dca7cbddfe27b4e6dee99c858c0
sophie-champagne <86581871+sophie-champagne@users.noreply.github.com>
2021-08-15 22:59:38 -0400

    Create 1_two_sum.py

---
+++ week2_study/1_two_sum.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        result = []
+        
+        #double pointers, loop through the i 
+        #loop through the j to j - 1 and find the match 
+        for i in range(len(nums)):
+            for j in range(i+1,len(nums)):
+                if nums[i] + nums[j] == target:
+                    return [i,j]
